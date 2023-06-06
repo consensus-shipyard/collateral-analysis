6a1a6003592f668da399e56c8c0bb6c20a40ec7f
Alejandro Ranchal-Pedrosa <a.ranchalpedrosa@gmail.com>
2023-06-06 23:58:30 +1000

    Simplify and beautify outputs

--- collateral-analysis.py
+++ collateral-analysis.py
@@ -53,21 +53,19 @@ def collateral_lower_bound(total_collateral, collateral, n):
     if int(collateral) == 0:
         return total_collateral 
 
-    return min(total_collateral, collateral*n)
+    return max(total_collateral, collateral*n)
 
 def main():
     dist, n, q, f, max_a, C, w, omega = parameters_from_input() #default_parameters_example1()
 
-    print("With the given parameters, the subnet is incentive-compatible against an attack into:\n")
-    for a in range(2,max_a+1):
-        c = C/n
-        slashable_collateral = minimum_adversary(a, n, q)*c
-        m = maximum_safe_spend(a, slashable_collateral, w, omega, dist)
-        print("{:<4} branches with {:.5f} coins, or".format(a, m))
+    c = C/n
+    slashable_collateral = minimum_adversary(max_a, n, q)*c #the hardest scenario is for max_a
+    m = maximum_safe_spend(max_a, slashable_collateral, w, omega, dist)
+    print("With the given parameters, the subnet is incentive-compatible against an attack that equivocates into {} branches with {:.3f} coins".format(max_a, m))
 
 def parameters_from_input():
     # If no, then draw from the default libp2p measurements lognormal mu=-1.0 sigma=1.0
-    distr_input = input("Would you like to follow the recommended message delay distribution (Y/n)")
+    distr_input = input("Would you like to follow the recommended message delay distribution (Y/n) ")
     if distr_input == 'n':
         print("We do not tolerate any other distribution for the moment")
         exit
@@ -78,26 +76,26 @@ def parameters_from_input():
 
     # At the moment, we assume no committee rotation (full participation)
     n = float(input("Enter the number of players in the committee: "))
-    q = float(input("Enter the quorum size q \in (n/2,n]: "))
-    f = float(input("Enter the rational adversary that should be tolerated f \in [0,q)"))
+    q = float(input("Enter the quorum size q \in ({},{}]: ".format(int(n/2),int(n))))
+    f = float(input("Enter the rational adversary that should be tolerated f \in [0,{}): ".format(int(q))))
     max_a = fork_max_branches(n, q, f)
     if max_a == 1:
-        print("That adversary is not big enough to double-spend given the quorum size")
+        print("That adversary is not big enough to double-spend given the quorum size: ")
         exit
 
-    print("The adversary can create a multiple spend into up to {} branches of a fork".format(max_a))
+    print("The size of this rational adversary means it can equivocate into up to {} distinct blocks in one iteration".format(max_a))
 
-    total_collateral_input = float(input("Enter the minimum total collateral"))
-    collateral_input = float(input("Enter the minimum collateral per process"))
+    total_collateral_input = float(input("Enter the minimum total collateral: "))
+    collateral_input = float(input("Enter the minimum collateral per process: "))
 
     C = collateral_lower_bound(total_collateral_input, collateral_input, n)
 
-    w_blocks = float(input("Enter the number of blocks between a request to retrieve collateral and when it is returned:"))
-    blocktime = float(input("Enter the expected block production time of the subnet (in seconds)"))
+    w_blocks = float(input("Enter the number of blocks between when a request to retrieve collateral is written in the parent subnet and when it is executed to unstake the collateral: "))
+    blocktime = float(input("Enter the expected block production time of the subnet (in seconds) "))
 
     w = blocktime * w_blocks
 
-    omega_blocks = float(input("Enter the number of blocks for an application to consider a transaction in the subnet as final (0 if immediate):"))
+    omega_blocks = float(input("Enter the number of blocks for an application to consider a transaction in the subnet as final (0 if immediate): "))
 
     omega = omega_blocks*blocktime
 
